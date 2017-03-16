d87edd038e0e7001fd882e22f24ce31054f7bc1c
DarkElement75 <blakeedwards777@gmail.com>
2017-03-16 15:18:39 -0600

    Documented methods and classes

--- lira/lira2/src/dataset_handler.py
+++ lira/lira2/src/dataset_handler.py
@@ -1,6 +1,7 @@
 """
-For easily getting a dataset object with the methods and objects we need to access easily in training.
+For easily getting a dataset object with the methods and objects we need to access easily in training a model.
 
+As many of the methods vary in their functionality, they each have their own documentation.
 -Blake Edwards / Dark Element
 """
 
@@ -9,14 +10,38 @@ import h5py
 import numpy as np
 
 def get_one_hot_m(v, width):
-    #Given vector and width of each one hot, 
-    #   get one hot matrix such that each index specified becomes a row in the matrix
+    """
+    Arguments: 
+        v: Vector of indices to go through when creating our one-hot matrix
+        width: The width to make each one-hot vector in our resulting matrix
+
+    Returns:
+        A matrix, where each ith row in the matrix is a one-hot vector of width `width` according to the ith index in `v`.
+        e.g. v = [0,3,2], width=5 
+            returns [1 0 0 0 0]
+                    [0 0 0 1 0]
+                    [0 0 1 0 0]
+        So that the final matrix is of shape (len(v), width)
+
+    Further documentation of this method can be found in keras's np_utils.to_categorical method.
+    """
     m = np.zeros(shape=(len(v), width))
     m[np.arange(len(v)), v] = 1
     return m
 
 def unison_shuffle(a, b):
-    #Shuffle our two arrays while retaining the relation between them
+    """
+    Arguments:
+        a, b: np arrays which share the same first dimension shape, 
+            e.g. a -> (60, 28, 28), b -> (60,)
+    
+    Returns:
+        Shuffles the two np arrays given in the exact same way, so that an element stored at an index in a with it's matching pair or label in the same index in b
+            will retain that relation even after this shuffle.
+        E.g. Before shuffle: a[4] = "some data", b[4] = 3
+             After shuffle:  a[random_new_index] = "some data", b[random_new_index] = 3
+        Does not return any values.
+    """
     rng_state = np.random.get_state()
     np.random.shuffle(a)
     np.random.set_state(rng_state)
@@ -28,15 +53,22 @@ def whole_normalize_data(data, mean, std):
 
 def get_data_subsets(archive_dir, p_training=0.8, p_validation=0.1, p_test=0.1):
     """
-    Get our dataset array and seperate it into training, validation, and test data
-        according to the percentages passed in.
-
-    Defaults:
-        80% - Training
-        10% - Validation
-        10% - Test
-
-    This one is our general function, which will work unless we have a strange dataset bias situation as in the LIRA example.
+    Arguments:
+        archive_dir: String where .h5 file is stored containing model's data.
+        p_training, p_validation, p_test: Percentage distribution of entire dataset to give to each subset.
+            These should sum to 1.
+            Defaults:
+                .8 / 80% - Training
+                .1 / 10% - Validation
+                .1 / 10% - Test
+        
+    Returns:
+        Loads the data stored in our archive_dir.
+            This dataset should contain two np arrays, x and y, of shapes 
+                x: (sample_n, ...)
+                y: (sample_n,)
+        From this data, and the number of samples, we divide the data by the proportions given 
+            into each data subset, and return these three subsets (training, validation, and test)
     """
 
     print "Getting Training, Validation, and Test Data..."
@@ -74,6 +106,25 @@ def get_data_subsets(archive_dir, p_training=0.8, p_validation=0.1, p_test=0.1):
     return training_data, validation_data, test_data
 
 def load_dataset_obj(p_training, p_validation, p_test, archive_dir, output_dims, whole_data_normalization=True):
+    """
+    Arguments:
+        p_training, p_validation, p_test: Percentage distribution of entire dataset to give to each subset.
+            These should sum to 1.
+            Defaults:
+                .8 / 80% - Training
+                .1 / 10% - Validation
+                .1 / 10% - Test
+        archive_dir: String where .h5 file is stored containing model's data.
+        output_dims: int for the number of output dimensions, or classes.
+        whole_data_normalization: boolean representing whether to mean & stddev normalize our data or not.
+
+    Returns:
+        Gets the data subsets from `archive_dir`, 
+        Normalizes them with mean & stddev normalization if enabled, 
+        Converts the y labels into one-hots,
+        then creates a Dataset object for easy reference of the data subsets,
+        and returns this Dataset object.
+    """
     training_data, validation_data, test_data = get_data_subsets(archive_dir, p_training=p_training, p_validation=p_validation, p_test=p_test)
         
     #Do whole data normalization on our input data, by getting the mean and stddev of the training data,
@@ -93,10 +144,22 @@ def load_dataset_obj(p_training, p_validation, p_test, archive_dir, output_dims,
     validation_data[1] = get_one_hot_m(validation_data[1], output_dims)
     test_data[1] = get_one_hot_m(test_data[1], output_dims)
 
-    #return dataset obj
     return Dataset(training_data, validation_data, test_data), whole_normalization_data
 
 class Dataset(object):
+    """
+    Our object for easy reference of data subsets and their x and y component parts.
+        when properly initialized, can be referenced via    
+            self.training -> training data DataSubset object
+            self.validation -> validation data DataSubset object
+            self.test -> test data DataSubset object
+            self.training.x -> input training data
+            self.training.y -> output training data
+            ||
+            \/ and so on...
+        Also has next_batch method for when training on one mini batch each epoch,
+            where the training dataset is shuffled and a mini batch subsection of it returned.
+    """
 
     #Initialize our data subset objects
     def __init__(self, training_data, validation_data, test_data):
