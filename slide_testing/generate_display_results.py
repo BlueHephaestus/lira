8485f6dfd3393aed45d6588dbff2b04f0e803b4a
DarkElement75 <blakeedwards777@gmail.com>
2017-03-17 14:58:04 -0600

    Improved into a program to generate our images given predictions and images, and documented

--- slide_testing/generate_display_results.py
+++ slide_testing/generate_display_results.py
@@ -1,66 +1,98 @@
+"""
+This file is mainly for generating human-viewable images from completed predictions, given greyscale images.
+Usually run after generate_predictions.py. 
+Further documentation can be found in each method.
+
+-Blake Edwards / Dark Element
+"""
 import sys, h5py, pickle, cv2
-import numpy as np
-
-
-sys.path.append("../../markov_random_fields/MRF/")
-
-import mrf_denoiser
-
-sys.path.append("../slide_testing/")
 
 import img_handler
-from img_handler import *
+from img_handler import get_relative_factor
 
 sys.path.append("../slide_labeller/")
 
 import subsection_handler
-from subsection_handler import *
+from subsection_handler import get_next_overlay_subsection
 
-class_n = 7
+def generate_display_results(img_archive_dir = "../lira/lira1/data/greyscales.h5", predictions_archive_dir = "../lira/lira1/data/predictions.h5", classification_metadata_dir = "classification_metadata.pkl", results_dir = "results"):
+    """
+    Arguments:
+        img_archive_dir: a string filepath of the .h5 file where the images / greyscales are stored.
+        predictions_archive_dir: a string filepath of the .h5 file where the model's predictions on the images/greyscales is stored.
+        classification_metadata_dir: a string filepath of the .pkl file where the model's classification strings and color key for each classification are stored.
+        results_dir: a string filepath to store each result image.
 
-img_archive_dir="../lira/lira1/data/greyscales.h5"
-predictions_archive_dir="../lira/lira1/data/predictions.h5"
-classification_metadata_dir="../slide_testing/classification_metadata.pkl"
+    Returns:
+        Loops through each image in the `img_archive_dir`, as well as each prediction in the `predictions_archive_dir`. There should be one prediction for each image.
+        Then loops through each image's subsections, 
+        Generates an overlay subsection from the image and it's associated prediction,
+        Resizes accordingly,
+        And saves the image to the results_dir.
 
-results_dir = "../slide_testing/results"
+        Has no return values.
+    """
 
-with h5py.File(img_archive_dir, "r") as img_hf:
-    with h5py.File(predictions_archive_dir, "r+") as predictions_hf:
-        img_n = len(img_hf.keys())
-
-        f = open(classification_metadata_dir, "r")
-        classifications, colors = pickle.load(f)
-        f.close()
-
-        for img_i in range(img_n):
-            print img_i
-            img = img_hf.get(str(img_i))
-            img_predictions = predictions_hf.get(str(img_i))
-
-            resize_factor = 1/float(get_relative_factor(img.shape[0], None))
-            #print np.array(img_predictions)
-            #print np.array(img_predictions).shape
-            #img_predictions = mrf_denoiser.denoise(np.array(img_predictions), class_n)
-            """
-            ATTENTION
-            Apparently just doing img_predictions = "whatever" is not enough, you have to reference the inside elements so as not to break the reference.
-            So we do img_predictions[...] = "whatever instead.
-            """
-            img_predictions[...] = mrf_denoiser.denoise(np.array(img_predictions), class_n)
-            print ""
-            #predictions_hf.create_dataset(str(img_i), data=img_predictions)#if the first bit doesn't work
+    """
+    Open our image and prediction files for iteration
+    """
+    with h5py.File(img_archive_dir, "r") as img_hf:
+        with h5py.File(predictions_archive_dir, "r") as predictions_hf:
 
             """
-            Then we loop through the subs and get each overlay subsection, then save it. 
-            This is only temporary until we upgrade generate_overlay to do the same or make our own script that is separate from it.
+            Get image number for iteration
             """
+            img_n = len(img_hf.keys())
 
-            factor = get_relative_factor(img.shape[0], None)
-            sub_n = factor**2
-            for sub_i in range(sub_n):
-                row_i = sub_i//factor
-                col_i = sub_i % factor
+            """
+            Get our classification metadata for generating the overlay subsection later.
+            """
+            f = open(classification_metadata_dir, "r")
+            classifications, colors = pickle.load(f)
+            f.close()
 
-                overlay_sub = get_next_overlay_subsection(img_i, sub_i, factor, img, img_predictions, classifications, colors, alpha=0.33, sub_h=80, sub_w=145)
-                overlay_sub = cv2.resize(overlay_sub, (0,0), fx=resize_factor, fy=resize_factor)
-                cv2.imwrite('%s/%i_%i_%i.jpg' % (results_dir, img_i, row_i, col_i), overlay_sub)
+            """
+            Start looping through images
+            """
+            for img_i in range(img_n):
+                """
+                Get the image and predictions for the image
+                """
+                img = img_hf.get(str(img_i))
+                img_predictions = predictions_hf.get(str(img_i))
+
+                """
+                Get the factor to use for both subsections and resizing
+                """
+                factor = get_relative_factor(img.shape[0], None)
+                sub_n = factor**2
+
+                """
+                Start looping through subsections
+                """
+                for sub_i in range(sub_n):
+                    """
+                    Get row index and column index given our subsection index and factor
+                    """
+                    row_i = sub_i//factor
+                    col_i = sub_i % factor
+
+                    """
+                    Get our resize / scale factor from our division factor
+                    """
+                    resize_factor = 1./factor
+
+                    """
+                    Get our overlay subsection
+                    """
+                    overlay_sub = get_next_overlay_subsection(img_i, sub_i, factor, img, img_predictions, classifications, colors, alpha=0.33, sub_h=80, sub_w=145)
+
+                    """
+                    Resize the overlay subsection with our resize_factor
+                    """
+                    overlay_sub = cv2.resize(overlay_sub, (0,0), fx=resize_factor, fy=resize_factor)
+
+                    """
+                    Finally, write the image
+                    """
+                    cv2.imwrite('%s/%i_%i_%i.jpg' % (results_dir, img_i, row_i, col_i), overlay_sub)
