5d2ff017acb6aaff26c25477a4def26a4a314daa
DarkElement75 <blakeedwards777@gmail.com>
2017-03-17 16:12:21 -0600

    Modified to include generate_display_results in pipeline

--- update_predictions.py
+++ update_predictions.py
@@ -3,8 +3,9 @@ Meta file to make use of the major stages of LIRA in order to do:
     1. Raw samples -> Sample archive (get_archive.py)
     2. Raw Greyscales -> Greyscale archive (get_greyscales.py)
     3. Sample archive -> New trained model (experiment_config.py)
-    4. Trained model & Greyscale archive -> Predictions (generate_overlay.py)
-    5. Predictions -> Jpg predictions per image (concatenate_results.py)
+    4. Trained model & Greyscale archive -> Predictions Archive (generate_predictions.py)
+    5. Predictions Archive & Greyscale Archive -> Jpg predictions per image per subsection (generate_display_results.py)
+    6. Jpg predictions per image per subsection -> Jpg predictions per image (concatenate_results.py)
 
 We have to set the directories again in the arguments because of where this file is positioned. When we have "../" something in one of our files for the directory, it is relative to the parent file calling that function. When that function is here, i.e. not in the normal place, it messes up stuff sometimes.
 
@@ -16,27 +17,19 @@ Good luck, have fun!
 
 -Blake Edwards / Dark Element
 """
-"""
-OI FUTURE SELF
-    Alright so our overlays are entirely pink, 
-    and our accuracies are entirely too perfect.
-    I think I see the problem here.
-    We aren't importing our data into a dataset object correctly when using live_samples.h5 (and no other samples)
-    It seems to be just storing them all as one label.
-    Get them to use the metadata we have if you can, and fixing that should fix the rest of it.
-    Good luck, have fun!
-"""
 
 import sys
 
 sys.path.append("lira/lira1/src")
 
 import get_archive
+import get_greyscales
 import experiment_config
 
 sys.path.append("slide_testing")
 
-import generate_overlay
+import generate_predictions
+import generate_display_results
 import concatenate_results
 
 def main(nn_title):
@@ -56,7 +49,7 @@ def main(nn_title):
         (If we want to, and it isn't a massive amount of greyscales we want to avoid)
     """
     print "Getting Greyscales Archive..."
-    #get_greyscales.load_greyscales("../data/test_slides", "../data/greyscales.h5")
+    #get_greyscales.load_greyscales("lira/lira1/data/test_slides", "lira/lira1/data/greyscales.h5")
 
     """
     From our samples.h5 file, train our model and save in saved_networks/`nn`
@@ -68,11 +61,16 @@ def main(nn_title):
     From our saved model and greyscales, generate new predictions.h5 file
     """
     print "Generating Predictions..."
-    #generate_overlay.generate_predictions(nn)
-    generate_overlay.generate_predictions(nn, nn_dir = "lira/lira1/src", img_archive_dir = "lira/lira1/data/greyscales.h5", predictions_archive_dir = "lira/lira1/data/predictions.h5", classification_metadata_dir = "slide_testing/classification_metadata.pkl", results_dir = "slide_testing/results")
+    generate_predictions.generate_predictions(nn, nn_dir = "lira/lira1/src", img_archive_dir = "lira/lira1/data/greyscales.h5", predictions_archive_dir = "lira/lira1/data/predictions.h5", classification_metadata_dir = "slide_testing/classification_metadata.pkl", results_dir = "slide_testing/results")
 
     """
-    Concatenate results of generating overlay, for showing off and debugging if we want to
+    From our new predictions.h5 and greyscales, generate human accessible images for viewing.
+    """
+    print "Generating Display Results..."
+    generate_display_results.generate_display_results(img_archive_dir = "lira/lira1/data/greyscales.h5", predictions_archive_dir = "lira/lira1/data/predictions.h5", classification_metadata_dir = "slide_testing/classification_metadata.pkl", results_dir = "slide_testing/results")
+
+    """
+    Concatenate results of generating display results, for showing off and/or debugging
     """
     print "Concatenating Results..."
     concatenate_results.concatenate_results("slide_testing/results/", "slide_testing/concatenated_results/", classification_metadata_dir="slide_testing/classification_metadata.pkl")
@@ -82,4 +80,4 @@ def main(nn_title):
     """
     print "Completed! -DE"
 
-main("New LIRA Live Data MK1_1")
+main("New LIRA Live Data and BBHO Parameters MK1_2")
