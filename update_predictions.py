8e5c494490072feda8449bc1fcfe3690615271e2
DarkElement75 <blakeedwards777@gmail.com>
2017-04-22 11:57:51 -0600

    Upgrading to use RGB pipeline

--- update_predictions.py
+++ update_predictions.py
@@ -22,7 +22,9 @@ import sys
 sys.path.append("lira/lira2/src")
 
 import get_greyscales
-import lira2
+import get_archive
+import lira2_pre_transfer_learning
+#import lira2_pre_transfer_learning_rgb
 
 sys.path.append("lira_static")
 
@@ -42,34 +44,37 @@ def main(model_title):
     """
     print "Getting Greyscales Archive..."
     #get_greyscales.load_greyscales("lira/lira1/data/test_slides", "lira/lira1/data/greyscales.h5")
+    get_archive.create_archive("lira/lira1/data/test_slides", "lira/lira1/data/images.h5", rgb=True)
 
     """
     From our samples.h5 file, train our model and save in saved_networks/`nn`
     """
     print "Training Model..."
-    lira2.train_model(model, model_dir="lira/lira2/saved_networks", archive_dir="lira/lira2/data/live_samples.h5")
+    #lira2_pre_transfer_learning.train_model(model, model_dir="lira/lira2/saved_networks", archive_dir="lira/lira2/data/augmented_samples.h5")
+    #lira2_pre_transfer_learning_rgb.train_model(model, model_dir="lira/lira2/saved_networks", archive_dir="lira/lira2/data/augmented_samples.h5")
+
 
     """
     From our saved model and greyscales, generate new predictions.h5 file
     """
     print "Generating Predictions..."
-    generate_predictions.generate_predictions(model, model_dir = "lira/lira2/saved_networks", img_archive_dir = "lira/lira1/data/greyscales.h5", predictions_archive_dir = "lira/lira1/data/predictions.h5", classification_metadata_dir = "lira_static/classification_metadata.pkl")
+    #generate_predictions.generate_predictions(model, model_dir = "lira/lira2/saved_networks", img_archive_dir = "lira/lira1/data/greyscales.h5", predictions_archive_dir = "lira/lira1/data/predictions.h5", classification_metadata_dir = "lira_static/classification_metadata.pkl")
 
     """
     From our new predictions.h5 and greyscales, generate human accessible images for viewing.
     """
     print "Generating Display Results..."
-    generate_display_results.generate_display_results(img_archive_dir = "lira/lira1/data/greyscales.h5", predictions_archive_dir = "lira/lira1/data/predictions.h5", classification_metadata_dir = "lira_static/classification_metadata.pkl", results_dir = "lira_static/results")
+    #generate_display_results.generate_display_results(img_archive_dir = "lira/lira1/data/greyscales.h5", predictions_archive_dir = "lira/lira1/data/predictions.h5", classification_metadata_dir = "lira_static/classification_metadata.pkl", results_dir = "lira_static/results", neighbor_weight=0.8, epochs=0)
 
     """
     Concatenate results of generating display results, for showing off and/or debugging
     """
     print "Concatenating Results..."
-    concatenate_results.concatenate_results("lira_static/results/", "lira_static/concatenated_results/", classification_metadata_dir="lira_static/classification_metadata.pkl")
+    #concatenate_results.concatenate_results("lira_static/results/", "lira_static/concatenated_results/", classification_metadata_dir="lira_static/classification_metadata.pkl")
     
     """
     At this point we have all we need to open LIRA live again when we want to, so we are done!
     """
     print "Completed! -DE"
 
-main("LIRA MK2")
+main("LIRA MK2 RGB dataset tests")
