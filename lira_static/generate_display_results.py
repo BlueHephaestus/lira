2ba5e705fd034d7c5da081e055cf114eac7bb76b
DarkElement75 <blakeedwards777@gmail.com>
2017-05-27 01:17:50 -0600

    Drastically remodeled and upgraded to be simpler and more efficient, initial commit. Should make concatenate_results obsolete once fully functional.

--- lira_static/generate_display_results.py
+++ lira_static/generate_display_results.py
@@ -10,7 +10,7 @@ import os, sys, h5py, pickle, cv2
 import numpy as np
 
 import img_handler
-from img_handler import get_relative_factor, clear_dir
+from img_handler import *
 
 sys.path.append(os.path.expanduser("~/programming/machine_learning/tuberculosis_project/lira_live/"))
 
@@ -19,13 +19,15 @@ from subsection_handler import get_next_overlay_subsection
 
 import post_processing
 
-def generate_display_results(img_archive_dir = "../lira/lira1/data/greyscales.h5", predictions_archive_dir = "../lira/lira1/data/predictions.h5", classification_metadata_dir = "classification_metadata.pkl", results_dir = "results", neighbor_weight=0.8, epochs=4, rgb=False):
+def generate_display_results(img_archive_dir = "../lira/lira1/data/greyscales.h5", predictions_archive_dir = "../lira/lira1/data/predictions.h5", classification_metadata_dir = "classification_metadata.pkl", results_dir = "results", alpha=.33, sub_h=80, sub_w=145, neighbor_weight=0.8, epochs=4, rgb=False):
     """
     Arguments:
         img_archive_dir: a string filepath of the .h5 file where the images / greyscales are stored.
         predictions_archive_dir: a string filepath of the .h5 file where the model's predictions on the images/greyscales is stored.
         classification_metadata_dir: a string filepath of the .pkl file where the model's classification strings and color key for each classification are stored.
         results_dir: a string filepath to store each result image.
+        alpha: transparency weight of our overlay, percentage b/w 0 and 1, with 0 being no overlay and 1 being only overlay.
+        sub_h, sub_w: The size of our individual subsections.
         neighbor_weight: 
             How much importance to put on the neighbor values. 
             This could also be thought of as a smoothing factor.
@@ -36,10 +38,10 @@ def generate_display_results(img_archive_dir = "../lira/lira1/data/greyscales.h5
     Returns:
         Loops through each image in the `img_archive_dir`, as well as each prediction in the `predictions_archive_dir`. There should be one prediction for each image.
         Denoises the predictions for the image,
-        Then loops through each image's subsections, 
-        Generates an overlay subsection from the image and it's associated prediction,
-        Resizes accordingly,
-        And saves the image to the results_dir.
+        Resizes our image to be small enough so we can open it,
+        Creates a colored rectangle on an overlay for each prediction,
+        Then combines the overlay and image, using alpha for the weight to put on the overlay.
+        This final overlay image is saved to the results_dir.
 
         Has no return values.
 
@@ -95,48 +97,118 @@ def generate_display_results(img_archive_dir = "../lira/lira1/data/greyscales.h5
                 Get the image and predictions for the image
                 """
                 img = img_hf.get(str(img_i))
-                img_predictions = np.array(predictions_hf.get(str(img_i)))
+                predictions = np.array(predictions_hf.get(str(img_i)))
 
+                print predictions.shape
                 """
                 We then denoise our predictions, now that all the predictions are loaded for this image.
                 """
-                img_predictions = post_processing.denoise_predictions(img_predictions, neighbor_weight, epochs)
+                predictions = post_processing.denoise_predictions(predictions, neighbor_weight, epochs)
 
                 """
-                Get the factor to use for both subsections and resizing
+                Get the factor to use for resizing
                 """
                 factor = get_relative_factor(img.shape[0], None)
-                sub_n = factor**2
 
                 """
-                Start looping through subsections
+                Get our resize / scale factor from our division factor
                 """
-                for sub_i in range(sub_n):
-                    sys.stdout.write("\rIMAGE %i, SUBSECTION %i" % (img_i, sub_i))
-                    sys.stdout.flush()
+                resize_factor = 1./factor
 
-                    """
-                    Get row index and column index given our subsection index and factor
-                    """
-                    row_i = sub_i//factor
-                    col_i = sub_i % factor
+                """
+                So we want to generate the entire overlay at once. 
+                In order to do this, we can't actually make it at full resolution and THEN
+                    resize it down to the resized resolution, since
+                    often times our images are too big to hold in memory.
 
-                    """
-                    Get our resize / scale factor from our division factor
-                    """
-                    resize_factor = 1./factor
+                So instead, we want to resize our images down immediately to their final size,
+                    and create our overlay at the final size.
 
-                    """
-                    Get our overlay subsection
-                    """
-                    overlay_sub = get_next_overlay_subsection(img_i, sub_i, factor, img, img_predictions, classifications, colors, alpha=0.33, sub_h=80, sub_w=145, rgb=rgb)
+                First part's easy, however the second is a bit trickier. 
+                
+                We want the rectangles in our overlay to be the same size as if we created the
+                    entire overlay, and then resized it down by resize_factor.
+                Since each rectangle is of size sub_h x sub_w, we can get the resulting size
+                    of each subsection via sub_h * resize_factor x sub_w * resize_factor.
+                However since we need to draw rectangles with integer values, we then 
+                    floor these calculations and then convert to int.
+                """
+                resized_sub_h = int(np.floor(sub_h * resize_factor))
+                resized_sub_w = int(np.floor(sub_w * resize_factor))
 
-                    """
-                    Resize the overlay subsection with our resize_factor
-                    """
-                    overlay_sub = cv2.resize(overlay_sub, (0,0), fx=resize_factor, fy=resize_factor)
+                """
+                Once we've got that, we're halfway there.
+                The reason we do this is because of the following problem. 
+               
+                Each time we draw a resized subsection rectangle, it will be of size
+                    resized_sub_h x resized_sub_w. 
+                
+                Since we draw the same amount of rectangles as predictions,
+                    we will have predictions.shape[0] x predictions.shape[1] rectangles,
+                    each one of shape resized_sub_h x resized_sub_w.
 
-                    """
-                    Finally, write the image
-                    """
-                    cv2.imwrite('%s/%i_%i_%i.jpg' % (results_dir, img_i, row_i, col_i), overlay_sub)
+                So we know that:
+                    the height of our overlay is resized_sub_h * predictions.shape[0]
+                    the width of our overlay is  resized_sub_w * predictions.shape[1]
+
+                However since we have to cast our resized_sub_h to integer in the calculation,
+                    it means we may slowly end up with an overlay with dimensions which
+                    don't match our image. This causes a mismatch between the images 
+                    once the entire overlay is created, and it's very ugly. 
+
+                Fortunately we can fix this. Using the height as the example dimension:
+
+                    img.shape[0] * fy = resized_sub_h * predictions.shape[0]
+
+                We can see this is the condition we want to be true. 
+                We want the resized dimension(s) of our original image to match
+                    the dimension(s) of our prediction overlay.
+                
+                So from this, we can see we already know everything but fy, 
+                    and since it's much easier to solve for fy than to figure out some
+                    special way to draw our subsection rectangles, we calculate:
+
+                    fy = (resized_sub_h * predictions.shape[0])/img.shape[0]
+
+                And we repeat that for fx, as well.
+                """
+                fy = (resized_sub_h * predictions.shape[0]) / float(img.shape[0])
+                fx = (resized_sub_w * predictions.shape[1]) / float(img.shape[1])
+
+                """
+                We then cast our img to nparray so we can properly resize,
+                    and then resize using our calculated fx and fy.
+                """
+                img = cv2.resize(np.array(img), (0,0), fx=fx, fy=fy)
+
+                sys.stdout.write("\rIMAGE %i" % img_i)
+                sys.stdout.flush()
+                """
+                In order to have integers for each prediction, 
+                    we argmax over the probability vectors for each prediction,
+                    giving us an integer. 
+                We then cast it as such.
+                """
+                predictions = np.argmax(predictions, axis=2)
+                predictions = predictions.astype(np.uint8)
+
+                """
+                Initialize our new color overlay to match our img
+                """
+                overlay = np.zeros((img.shape[0], img.shape[1], 3))
+
+                """
+                Then loop through each prediction in our predictions,
+                    using the row and col indices with the resized_sub_h and resized_sub_w for the location of our colored rectangle on our overlay,
+                    and use the prediction's value in our color key for the color of this rectangle.
+                """
+                for prediction_row_i, prediction_row in enumerate(predictions):
+                    for prediction_col_i, prediction in enumerate(prediction_row):
+                        color = colors[prediction]
+                        cv2.rectangle(overlay, (prediction_col_i*resized_sub_w, prediction_row_i*resized_sub_h), (prediction_col_i*resized_sub_w+resized_sub_w, prediction_row_i*resized_sub_h+resized_sub_h), color, -1)
+                
+                """
+                We then add the img and overlay together into a weighted overlay using our alpha and function for it.
+                """
+                overlay = add_weighted_overlay(img, overlay, alpha, rgb=rgb)
+                cv2.imwrite("%s%s%i.png" % (results_dir, os.sep, img_i), overlay)
